ae432a05272a44014f863321bb8dd49fbd233153
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2024-10-16 20:30:28 +0700

    Update task7.py

--- task7.py
+++ task7.py
@@ -1,3 +1,12 @@
+def Fibonach(n):
+    list_fi=[1, 1]
+    i, j=0, 1
+    while list_fi[i]+list_fi[j]<=n:
+        list_fi.append(list_fi[i]+list_fi[j])
+        i+=1
+        j+=1
+    return list_fi
+
 # с клавиатуры вводятся числа, ввод завершается числом 0.
 # Определить минимальное из введённых чисел Фибоначчи.
 # Вывести "нет", если чисел Фибоначчи в последовательности нет.
@@ -5,3 +14,20 @@
 # которая начинается с двух единиц и каждое следующее число
 # равно сумме двух предыдущих: 1, 1, 2, 3, 5, 8, 13, …
 
+a=int(input())
+lst=[]
+ma=0
+while a!=0:
+    if a>ma:
+        ma=a
+    lst.append(a)
+    a=int(input())
+list_fi=Fibonach(ma)
+min_fi=ma+1
+for num in lst:
+    if num in list_fi and num<min_fi:
+        min_fi=num
+if min_fi==ma+1:
+    print('нет')
+else:    
+    print(min_fi)  
